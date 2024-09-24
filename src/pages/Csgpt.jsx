c288b0d20f25da993d35d4a3cf884ed9d04bbb51
Nishikant <69532575+Nishikant00@users.noreply.github.com>
2024-09-24 10:43:44 +0530

    Update Csgpt.jsx

--- src/pages/Csgpt.jsx
+++ src/pages/Csgpt.jsx
@@ -34,7 +34,7 @@ export default function Csgpt() {
     setIsFirstTime(false)
 
     try {
-      const response = await fetch("https://e113-43-231-238-206.ngrok-free.app/query", {
+      const response = await fetch("https://0c29-43-231-238-206.ngrok-free.app/query", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
