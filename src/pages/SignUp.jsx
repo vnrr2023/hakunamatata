b8f38e42e21e9bed131b14ef252cefc61f544d85
Nishikant <69532575+Nishikant00@users.noreply.github.com>
2024-10-15 11:02:32 +0530

    Update SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ import { StarsBackground } from "../components/ui/stars-background"
 import { motion } from "framer-motion"
 import { useNavigate } from "react-router-dom"
 
-export const google_ngrok_url = "https://f1f4-43-231-238-206.ngrok-free.app"
+export const google_ngrok_url = "https://affb-43-231-238-206.ngrok-free.app"
 
 
 export default function SignUp() {
